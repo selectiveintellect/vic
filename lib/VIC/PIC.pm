2421c7e09e232a644e178051792aa72f66ea70ad
Vikas N Kumar <vikas@cpan.org>
2014-01-28 01:10:08 -0500

    Adding some code for the block handling.

--- lib/VIC/PIC.pm
+++ lib/VIC/PIC.pm
@@ -12,6 +12,8 @@ use XXX;
 has info => undef;
 has ast => {};
 
+sub throw_error { shift->parser->throw_error(@_); }
+
 sub got_uc_select {
     my ($self, $type) = @_;
     $type = lc $type;
@@ -19,8 +21,11 @@ sub got_uc_select {
     # assume supported type else return
     $self->info(VIC::PIC::Any->new($type));
     die "$type is not a supported chip" unless $self->info->type eq $type;
+    # set the defaults in case the headers are not provided by the user
     $self->ast->{org} = $self->info->org;
     $self->ast->{config} = $self->info->config;
+    $self->ast->{block_stack} = [];
+    $self->ast->{block_stack_top} = 0;
     return;
 }
 
@@ -39,9 +44,31 @@ sub got_uc_header {
     return;
 }
 
+sub got_block {
+    my ($self, $list) = @_;
+    $self->flatten($list); # we flatten because we only want the name out
+    my $block = shift @$list;
+    push @{$self->ast->{block_stack}}, $block;
+    $self->ast->{block_stack_top} = scalar @{$self->ast->{block_stack}};
+    my $stack = [];
+    if ($block eq 'Main') {
+        push @$stack, "_start:";
+    }
+    $self->ast->{$block} = $stack;
+}
+
+sub got_end_block {
+    my ($self, $list) = @_;
+    # we are not capturing anything here
+    my $block = pop @{$self->ast->{block_stack}};
+    $self->ast->{block_stack_top} = scalar @{$self->ast->{block_stack}};
+}
+
 sub final {
     my ($self, $got) = @_;
     my $ast = $self->ast;
+    $self->throw_error("Missing '}'") if $self->ast->{block_stack_top} ne 0;
+    $self->throw_error("Main not defined") unless defined $self->ast->{Main};
     my $pic = <<"...";
 #include <$ast->{uc_type}.inc>
 
@@ -49,6 +76,8 @@ $ast->{config};
 
 org $ast->{org};
 
+$ast->{Main};
+$ast->{Loop};
 ...
     return $pic;
 }
