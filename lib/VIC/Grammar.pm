071c721ea30f93d61ed7ea75ba58f14c985bc456
Vikas N Kumar <vikas@cpan.org>
2014-02-21 21:48:17 -0500

    updating grammar revision

--- lib/VIC/Grammar.pm
+++ lib/VIC/Grammar.pm
@@ -14,7 +14,7 @@ sub make_tree {
   {
     '+grammar' => 'vic',
     '+toprule' => 'program',
-    '+version' => '0.0.4',
+    '+version' => '0.0.5',
     'COMMA' => {
       '.rgx' => qr/\G,/
     },
