5f8f92bfe70503b5d098606659f5e94de62a01b6
Vikas N Kumar <vikas@cpan.org>
2014-11-20 12:27:56 -0500

    adding header/lkr include options explicitly to avoid hassle on Windows systems

--- lib/VIC.pm
+++ lib/VIC.pm
@@ -13,6 +13,7 @@ our $Debug = 0;
 our $Intermediate = 0;
 our $GPASM;
 our $GPLINK;
+our $GPUTILSDIR;
 
 our $VERSION = '0.18';
 $VERSION = eval $VERSION;
@@ -44,7 +45,7 @@ sub is_chip_supported { return VIC::Receiver::is_chip_supported(@_) };
 sub list_chip_features { return VIC::Receiver::list_chip_features(@_) };
 
 sub _load_gputils {
-    my ($gpasm, $gplink);
+    my ($gpasm, $gplink, $bindir);
     my ($stdo, $stde) = capture {
         eval {
             require Alien::gputils;
@@ -55,12 +56,20 @@ sub _load_gputils {
             if ($alien) {
                 $gpasm = $alien->gpasm;
                 $gplink = $alien->gplink;
+                $bindir = $alien->bin_dir;
             }
         }
         unless (defined $gpasm and defined $gplink) {
             print "Looking for gpasm and gplink in \$ENV{PATH}\n" if $Debug;
             $gpasm = which('gpasm');
             $gplink = which('gplink');
+        }
+        unless (defined $bindir) {
+            if ($gpasm) {
+                my @dirs = File::Spec->splitpath($gpasm);
+                pop @dirs if @dirs;
+                $bindir = File::Spec->catdir(@dirs) if @dirs;
+            }
         }
     };
     print STDOUT $stdo if ($Debug and $stdo);
@@ -69,11 +78,13 @@ sub _load_gputils {
     print STDERR "Using gplink: $gplink\n" if ($Debug and $gplink);
     $GPASM = $gpasm;
     $GPLINK = $gplink;
-    return wantarray ? ($gpasm, $gplink) : [$gpasm, $gplink];
+    $GPUTILSDIR = $bindir;
+    return wantarray ? ($gpasm, $gplink, $bindir) : [$gpasm, $gplink, $bindir];
 }
 
 sub gputils {
-    return ($GPASM, $GPLINK) if defined $GPASM and defined $GPLINK;
+    return ($GPASM, $GPLINK, $GPUTILSDIR) if (defined $GPASM and defined $GPLINK
+                                                and defined $GPUTILSDIR);
     return &_load_gputils();
 }
 
@@ -89,6 +100,12 @@ sub gplink {
     return $out[1];
 }
 
+sub bindir {
+    return $GPUTILSDIR if defined $GPUTILSDIR;
+    my @out = &_load_gputils();
+    return $out[2];
+}
+
 sub assemble($$) {
     my ($chip, $output) = @_;
     return unless defined $chip;
@@ -102,12 +119,32 @@ sub assemble($$) {
         $hexfile = $output . '.hex';
         $objfile = $output . '.o';
     }
-    my ($gpasm, $gplink) = VIC::gputils();
+    my ($gpasm, $gplink, $bindir) = VIC::gputils();
     unless (defined $gpasm and defined $gplink and -e $gpasm and -e $gplink) {
         die "Cannot find gpasm/gplink to compile $output into a hex file $hexfile.";
     }
-    my $gpasm_cmd = "$gpasm -p $chip -M -c $output";
-    my $gplink_cmd = "$gplink -q -m -o $hexfile $objfile ";
+    my ($inc1, $inc2) = ('', '');
+    if (defined $bindir) {
+        my @dirs = File::Spec->splitdir($bindir);
+        my $l = pop @dirs if @dirs;
+        if (defined $l and $l ne 'bin') {
+            push @dirs, $l; # return the last directory
+        }
+        my @includes = ();
+        my @linkers = ();
+        push @includes, File::Spec->catdir(@dirs, 'header');
+        push @linkers, File::Spec->catdir(@dirs, 'lkr');
+        push @includes, File::Spec->catdir(@dirs, 'share', 'gputils', 'header');
+        push @linkers, File::Spec->catdir(@dirs, 'share', 'gputils', 'lkr');
+        foreach (@includes) {
+            $inc1 .= " -I $_ " if -d $_;
+        }
+        foreach (@linkers) {
+            $inc2 .= " -I $_ " if -d $_;
+        }
+    }
+    my $gpasm_cmd = "$gpasm $inc1 -p $chip -M -c $output";
+    my $gplink_cmd = "$gplink $inc2 -q -m -o $hexfile $objfile ";
     system($gpasm_cmd) == 0 or die "Unable to run '$gpasm_cmd': $?";
     system($gplink_cmd) == 0 or die "Unable to run '$gplink_cmd': $?";
     1;
